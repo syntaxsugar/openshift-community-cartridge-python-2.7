e90efe5b8cb5c9ffe49f406230dae74e5f0cee99
ramr <ramr@redhat.com>
2013-01-29 18:24:04 -0800

    Fix sample application to run w/ wsgiref simple server if gevent is not installed and add action hooks + markers + readme to the template app.

--- template/app.py
+++ template/app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-import os, sys
+import imp
+import os
+import sys
 
 PYCART_DIR = ''.join(['python-', '.'.join(map(str, sys.version_info[:2]))])
 
@@ -9,6 +11,16 @@ try:
    execfile(zvirtenv, dict(__file__ = zvirtenv) )
 except IOError:
    pass
+
+
+def run_gevent_server(app, ip, port=8080):
+   from gevent.pywsgi import WSGIServer
+   WSGIServer((ip, port), app).serve_forever()
+
+
+def run_simple_httpd_server(app, ip, port=8080):
+   from wsgiref.simple_server import make_server
+   make_server(ip, port, app).serve_forever()
 
 
 #
@@ -21,12 +33,15 @@ except IOError:
 #  main():
 #
 if __name__ == '__main__':
-   from gevent.pywsgi import WSGIServer
-   import imp
-   zapp = imp.load_source('application', 'wsgi/application')
-
    ip   = os.environ['OPENSHIFT_INTERNAL_IP']
    port = 8080
+   zapp = imp.load_source('application', 'wsgi/application')
+
+   #  Use gevent if we have it, otherwise run a simple httpd server.
    print 'Starting WSGIServer on %s:%d ... ' % (ip, port)
-   WSGIServer((ip, port), zapp.application).serve_forever()
+   try:
+      run_gevent_server(zapp.application, ip, port)
+   except:
+      print 'gevent probably not installed - using default simple server ...'
+      run_simple_httpd_server(zapp.application, ip, port)
 
