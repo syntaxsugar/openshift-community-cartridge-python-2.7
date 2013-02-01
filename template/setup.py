44bb9b4cca9056592efcc9b86fd8f164d6fa1cea
ramr <smitram@gmail.com>
2013-01-31 16:30:05 -0800

    Bug fixes - use env vars instead of expanded paths in activate_virtenv + source specific env vars, add info on how to add client drivers for mongodb/mysql/postgresql.

--- template/setup.py
+++ template/setup.py
@@ -4,5 +4,12 @@ setup(name='YourAppName', version='1.0',
       description='OpenShift Python-2.7 Community Cartridge based application',
       author='Your Name', author_email='ramr@example.org',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['greenlet', 'gevent'],
+
+      #  Uncomment one or more lines below in the install_requires section
+      #  for the specific client drivers/modules your application needs.
+      install_requires=['greenlet', 'gevent',
+                        #  'MySQL-python',
+                        #  'pymongo',
+                        #  'psycopg2',
+      ],
      )
